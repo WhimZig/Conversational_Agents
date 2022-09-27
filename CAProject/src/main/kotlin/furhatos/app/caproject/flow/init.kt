de42275817c11d8d2427f30f44cd2d3361844b96
Nicolas Perez <nicolasarturopz@gmail.com>
2022-09-27 10:06:01 +0200

    Initial furhat upload

---
+++ CAProject/src/main/kotlin/furhatos/app/caproject/flow/init.kt
@@ -0,0 +1,17 @@
+package furhatos.app.caproject.flow
+
+import furhatos.app.caproject.flow.main.Idle
+import furhatos.app.caproject.setting.distanceToEngage
+import furhatos.app.caproject.setting.maxNumberOfUsers
+import furhatos.flow.kotlin.*
+import furhatos.flow.kotlin.voice.Voice
+
+val Init : State = state() {
+    init {
+        /** Set our default interaction parameters */
+        users.setSimpleEngagementPolicy(distanceToEngage, maxNumberOfUsers)
+        furhat.voice = Voice("Matthew")
+        /** start the interaction */
+        goto(Idle)
+    }
+}
